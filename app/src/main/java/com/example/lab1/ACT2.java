42b59e494465bbcd7fc3103ad725660742f57d14
nipunapamuditha <nipunapamuditha234@gmail.com>
2021-09-06 18:13:01 +0530

    commit 2

--- app/src/main/java/com/example/lab1/ACT2.java
+++ app/src/main/java/com/example/lab1/ACT2.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ACT2 extends AppCompatActivity {
-
+ //testing vomment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
