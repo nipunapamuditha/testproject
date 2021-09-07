a29a70d19c595898fd5cc22a9fca23012cafceb2
nipunapamuditha <nipunapamuditha234@gmail.com>
2021-09-07 15:58:29 +0530

    3rd commit

--- app/src/main/java/com/example/lab1/ACT2.java
+++ app/src/main/java/com/example/lab1/ACT2.java
@@ -2,16 +2,22 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ACT2 extends AppCompatActivity {
- //testing vomment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_act2);
 
+    Intent intent =getIntent();
+    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        int z=0;
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_act2);
+    TextView textView =findViewById(R.id.textView);
+    textView.setText(message);
+
     }
 }
--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.lab1.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sendMessage(View view)
+    {
+    Intent intent = new Intent(this,ACT2.class);
+    EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
+    }
+
 }
